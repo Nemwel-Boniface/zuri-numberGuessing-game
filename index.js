7f959df02da1e5f3ac38b6e73a2cc2f250ef9b48
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-03 19:12:49 +0000

    Update the code to include functionality torequest user name

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ start();
 function play(name, inc = 2, points = 0) {
   console.log(inc);
   let guess = Math.floor(Math.random() * inc) + 1;
-  console.log(guess);
-  let guessQuest = prompt(`could you guess the magic number? between 1 and ${inc}`);
-  parseInt(guessQuest);
+  // console.log(guess);
+  let guessQuest = parseInt(prompt(`${name} could you guess the magic number? between 1 and ${inc}`));
+  // parseInt(guessQuest);
   if(guessQuest == guess) {
     console.log('congratulations!');
-    play(inc + 1, points + 1);
+    play(name, inc + 1, points + 1);
   } else {
     console.log(`Sorry ${name} that was wrong. \nCorrect number was ${guess}. \nYour total points earned were ${points}`);
     let choice = prompt('Would you like to try the game again?');
